f05c14f9a8b799dc22d0713e3c8e257d0f2277cb
kshitijrajsharma <skshitizraj@gmail.com>
2023-04-05 13:50:32 +0545

    reference log

--- hot_fair_utilities/georeferencing.py
+++ hot_fair_utilities/georeferencing.py
@@ -1,28 +1,43 @@
 # Standard library imports
 import os
+from concurrent.futures import ThreadPoolExecutor
 from glob import glob
 from pathlib import Path
 
 # Third party imports
-# Third-party imports
 from osgeo import gdal
-from tqdm import tqdm
 
 from .utils import get_bounding_box
 
 
+def georeference_image(path, input_path, output_path, is_mask):
+    filename = Path(path).stem
+    in_file = f"{input_path}/{filename}.png"
+    out_file = f"{output_path}/{filename}.tif"
+
+    x_min, y_min, x_max, y_max = get_bounding_box(filename)
+
+    bands = [1] if is_mask else [1, 2, 3]
+
+    _ = gdal.Translate(
+        destName=out_file,
+        srcDS=in_file,
+        format="GTiff",
+        bandList=bands,
+        outputBounds=[x_min, y_max, x_max, y_min],
+        outputSRS="EPSG:3857",
+    )
+    _ = None
+
+
 def georeference(input_path: str, output_path: str, is_mask=False) -> None:
     """Perform georeferencing and remove the fourth band from images (if any).
-
     CRS of the georeferenced images will be EPSG:3857 ('WGS 84 / Pseudo-Mercator').
-
     Args:
         input_path: Path of the directory where the input data are stored.
         output_path: Path of the directory where the output data will go.
         is_mask: Whether the image is binary or not.
-
     Example::
-
         georeference(
             "data/prediction-dataset/5x5/1-19",
             "data/georeferenced_input/1-19"
@@ -30,28 +45,8 @@ def georeference(input_path: str, output_path: str, is_mask=False) -> None:
     """
     os.makedirs(output_path, exist_ok=True)
 
-    for path in tqdm(
-        glob(f"{input_path}/*.png"), desc=f"Georeferencing for {Path(input_path).stem}"
-    ):
-        filename = Path(path).stem
-        in_file = f"{input_path}/{filename}.png"
-        out_file = f"{output_path}/{filename}.tif"
+    image_paths = glob(f"{input_path}/*.png")
 
-        # Get bounding box in EPSG:3857
-        x_min, y_min, x_max, y_max = get_bounding_box(filename)
-
-        # Use one band for masks and the first three bands for images
-        bands = [1] if is_mask else [1, 2, 3]
-
-        # Georeference image
-        # Output bounds are defined by upper left and lower right corners
-        _ = gdal.Translate(
-            destName=out_file,
-            srcDS=in_file,
-            format="GTiff",
-            bandList=bands,
-            outputBounds=[x_min, y_max, x_max, y_min],
-            outputSRS="EPSG:3857",
-        )
-        # Close dataset
-        _ = None
+    with ThreadPoolExecutor() as executor:
+        for path in image_paths:
+            executor.submit(georeference_image, path, input_path, output_path, is_mask)
--- hot_fair_utilities/inference/predict.py
+++ hot_fair_utilities/inference/predict.py
@@ -1,5 +1,6 @@
 # Standard library imports
 import os
+import time
 from glob import glob
 from pathlib import Path
 
@@ -14,6 +15,29 @@ from .utils import open_images, save_mask
 BATCH_SIZE = 8
 IMAGE_SIZE = 256
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+
+# Standard library imports
+import concurrent.futures
+import math
+
+
+def process_batch(image_batch, model, prediction_path):
+    images = open_images(image_batch)
+    images = images.reshape(-1, IMAGE_SIZE, IMAGE_SIZE, 3)
+
+    preds = model.predict(images)
+    preds = np.argmax(preds, axis=-1)
+    preds = np.expand_dims(preds, axis=-1)
+    preds = np.where(preds > 0.5, 1, 0)
+
+    for idx, path in enumerate(image_batch):
+        save_mask(
+            preds[idx],
+            str(f"{prediction_path}/{Path(path).stem}.png"),
+        )
+    del images
+    del preds
 
 
 def predict(checkpoint_path: str, input_path: str, prediction_path: str) -> None:
@@ -37,31 +61,31 @@ def predict(checkpoint_path: str, input_path: str, prediction_path: str) -> None
             "data/predictions/4"
         )
     """
+    start = time.time()
+
     model = keras.models.load_model(checkpoint_path)
+    print(f"It took {time.time()-start} sec to load model")
+    start = time.time()
 
     os.makedirs(prediction_path, exist_ok=True)
     image_paths = glob(f"{input_path}/*.png")
+    n_batches = math.ceil(len(image_paths) / BATCH_SIZE)
 
-    for i in range((len(image_paths) + BATCH_SIZE - 1) // BATCH_SIZE):
-        image_batch = image_paths[BATCH_SIZE * i : BATCH_SIZE * (i + 1)]
-        images = open_images(image_batch)
-        images = images.reshape(-1, IMAGE_SIZE, IMAGE_SIZE, 3)
-
-        preds = model.predict(images)
-        preds = np.argmax(preds, axis=-1)
-        preds = np.expand_dims(preds, axis=-1)
-        preds = np.where(preds > 0.5, 1, 0)
-
-        for idx, path in enumerate(image_batch):
-            save_mask(
-                preds[idx],
-                str(f"{prediction_path}/{Path(path).stem}.png"),
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for i in range(n_batches):
+            image_batch = image_paths[BATCH_SIZE * i : BATCH_SIZE * (i + 1)]
+            futures.append(
+                executor.submit(process_batch, image_batch, model, prediction_path)
             )
-        del images
-        del preds
-    keras.backend.clear_session()
-    del model
+
+        concurrent.futures.wait(futures)
+    print(f"It took {time.time()-start} sec for prediction")
+    start = time.time()
 
     georeference(prediction_path, prediction_path, is_mask=True)
+    print(f"It took {time.time()-start} sec for Georeference")
+    start = time.time()
+
     remove_files(f"{prediction_path}/*.xml")
     remove_files(f"{prediction_path}/*.png")
