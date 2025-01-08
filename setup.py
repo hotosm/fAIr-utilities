4e8296d9698588c4862a28a15bd3f5afa2878e1b
kshitijrajsharma <skshitizraj@gmail.com>
2025-01-08 15:11:08 +0100

    fix(version): fixes sub packages not being included in the packaging

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
-from setuptools import setup
+# Third party imports
+from setuptools import find_packages, setup
 
-setup()
+setup(
+    package_dir={"": "."},
+    packages=find_packages(include=["hot_fair_utilities", "hot_fair_utilities.*"]),
+    include_package_data=True,
+)
