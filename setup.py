164261775a42e536fef897868f76336ddc8c940e
LH <L.s.hamnett@gmail.com>
2025-03-27 17:45:08 +0000

    sync setup.py with main

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-from setuptools import setup, find_packages
-import os
-
-if os.path.exists('./ramp-code'):
-    print('ramp-code exists, starting setup')
-else:
-    print('ramp-code not found, cloning from github')
-    os.system('git clone https://github.com/kshitijrajsharma/ramp-code-fAIr.git ./ramp-code')
+# Third party imports
+from setuptools import find_packages, setup
 
 setup(
     package_dir={"": "."},
-    packages=find_packages(include=["hot_fair_utilities", "hot_fair_utilities.*", "ramp", "ramp.*"]),
+    packages=find_packages(include=["hot_fair_utilities", "hot_fair_utilities.*"]),
     include_package_data=True,
 )
